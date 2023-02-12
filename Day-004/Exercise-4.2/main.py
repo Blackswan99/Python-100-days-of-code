25445b46bc97a3014e0cb61f6e9bf5cccfec766a
Blackswan99 <blackswan99@gmx.de>
2023-02-12 19:39:12 +0100

    Update main.py

--- Day-004/Exercise-4.2/main.py
+++ Day-004/Exercise-4.2/main.py
@@ -1,1 +1,11 @@
-#
+# Import the random module here
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# Don't change the code above
+
+#Write your code below this line
+idx = random.randint(1,len(names))
+buys_the_meal = names[idx]
+print(f"{buys_the_meal} is going to buy the meal today!")
