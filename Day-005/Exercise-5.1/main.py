45bbaf7d16d9bb8b5d048fd08a55979336667331
Blackswan99 <blackswan99@gmx.de>
2023-02-12 22:12:50 +0100

    Update main.py

--- Day-005/Exercise-5.1/main.py
+++ Day-005/Exercise-5.1/main.py
@@ -1,1 +1,12 @@
-#
+# Don't change the code below
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# Don't change the code above
+
+
+#Write your code below this row
+sum = 0
+for height in student_heights:
+    sum += height
+print(round(sum / len(student_heights)))
