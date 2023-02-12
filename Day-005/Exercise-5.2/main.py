2b60e34cf655c52ddb400848cb2e3e63c0598ec9
Blackswan99 <blackswan99@gmx.de>
2023-02-12 22:18:53 +0100

    Create main.py

---
+++ Day-005/Exercise-5.2/main.py
@@ -0,0 +1,14 @@
+# Don't change the code below
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# Don't change the code above
+
+#Write your code below this row
+max = 0
+for score in student_scores:
+    if max < score:
+        max = score
+
+print(f"The highest score in the class is: {max}")
