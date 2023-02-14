36b28d5026b9476b4641a94b9235d4f6dbfa77ad
Blackswan99 <blackswan99@gmx.de>
2023-02-14 21:05:22 +0100

    Update main.py

--- Day-007/Challenge-1/main.py
+++ Day-007/Challenge-1/main.py
@@ -6,7 +6,7 @@ word_list = ["aardvark", "baboon", "camel"]
 rnd_idx = random.randint(0,2)
 chosen_word = word_list[rnd_idx]
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = (input("Guess a letter: ")).lower()
+guess = input("Guess a letter: ").lower()
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 for c in chosen_word:
     if c == guess:
