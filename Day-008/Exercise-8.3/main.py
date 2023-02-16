b8ac6867d9fcabf4dfcaae76789774dfabfe28e7
Blackswan99 <blackswan99@gmx.de>
2023-02-16 21:54:47 +0100

    Create main.py

---
+++ Day-008/Exercise-8.3/main.py
@@ -0,0 +1,26 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(message, shift):
+    encrypted_message = [''] * len(message)
+    idx = 0
+    for char in message:
+        position = alphabet.index(char)
+        position_new = position + shift
+        if position_new > 25:
+            position_new -= 26
+        char_new = alphabet[position_new]
+        encrypted_message[idx] = char_new
+        idx += 1
+    print(encrypted_message)
+encrypt(text, shift)
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
