5c2f187f6df607f1668dba1f6972cdedf0a047b8
Blackswan99 <blackswan99@gmx.de>
2023-02-26 13:27:45 +0100

    Update main.py

--- Day-016/Exercise-16.1/main.py
+++ Day-016/Exercise-16.1/main.py
@@ -1,8 +1,10 @@
-from turtle import Turtle,Screen
+from turtle import Turtle, Screen
 
 timmy = Turtle()
 #print(timmy)
 timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
 
 myScreen = Screen()
-myScreen.exitonclick(
+myScreen.exitonclick()
