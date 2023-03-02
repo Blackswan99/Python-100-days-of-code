9c11c5519f734908ad7c1351725fedbcaa657704
Blackswan99 <blackswan99@gmx.de>
2023-03-02 20:22:11 +0100

    Update main.py

--- Day-018/Exercise-18.1/main.py
+++ Day-018/Exercise-18.1/main.py
@@ -1,1 +1,14 @@
-#
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.color("coral")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+
+screen = Screen()
+screen.exitonclick()
