a3fd69a43d986eb21d314315b5547f4b8da92dc2
Blackswan99 <blackswan99@gmx.de>
2023-03-12 17:51:18 +0100

    Create main.py

---
+++ Day-026/Exercise-26.1/main.py
@@ -0,0 +1,14 @@
+# List comprehension
+numbers = [1, 2, 3, 4, 5, 6]
+new_numbers = [num for num in numbers]
+print(new_numbers)
+
+# More list comprehension
+numbers = [1, 2, 4, 8, 16, 32]
+new_numbers = [num * 2 for num in numbers]
+print(new_numbers)
+
+# Conditional list comprehension
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+short_names = [name for name in names if len(name) < 5]
+print(short_names)
