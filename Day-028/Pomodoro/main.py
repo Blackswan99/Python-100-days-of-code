8193cd9f7bc12611e5482ca6f453f6bb36f36d84
Blackswan99 <blackswan99@gmx.de>
2023-03-16 18:11:18 +0100

    Update main.py

--- Day-028/Pomodoro/main.py
+++ Day-028/Pomodoro/main.py
@@ -22,10 +22,10 @@ window.config(padx=90, pady=50, bg=YELLOW)
 
 
 photo = PhotoImage(file="tomato.png")
-canvas = Canvas(width=200, height=224, bg=YELLOW)
-canvas.create_image(101, 113, image=photo)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas.create_image(100, 112, image=photo)
 
-canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 15, "bold"))
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 15, "bold"))
 canvas.pack()
 
 window.mainloop()
