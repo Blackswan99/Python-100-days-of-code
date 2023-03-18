075a2bc61b98a993a99afb164870f36883b793f8
Blackswan99 <blackswan99@gmx.de>
2023-03-18 09:02:18 +0100

    Update main.py

--- Day-028/Pomodoro/main.py
+++ Day-028/Pomodoro/main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,9 +13,22 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def start_timer():
+    count_down(5 * 60)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,12 +41,21 @@ title_label.grid(column=1, row=0)
 photo = PhotoImage(file="tomato.png")
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=photo)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 20, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 20, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start")
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
-reset_button = Button(text="Reset")
-reset_button.grid(column=2, row=2)
-window.mainloop()
 
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✓", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+
+
+
+
+
+window.mainloop()
